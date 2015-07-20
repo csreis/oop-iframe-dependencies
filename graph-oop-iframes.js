a53dd127af9e77bdb1e34fbd13d9c11f0cdd7d82
Charles Reis <csreis@users.noreply.github.com>
2015-07-20 11:21:28 -0700

    Update graph-oop-iframes.js
    Reorganize

--- graph-oop-iframes.js
+++ graph-oop-iframes.js
@@ -2,29 +2,35 @@ loadData(
     {
         name: 'Chromium OOP iframes dependency graph',
         nodes: [
-            { id: 'oopiframes', value: { label: 'Out-of-process iframes', bug: '99379', milestone: true } },
-
-            { id: 'extensionLaunch', value: { label: 'Extension milestone', milestone: true } },
+            { id: 'extensionTrialLaunch', value: { label: 'Extension trial launch', milestone: true } },
+            { id: 'extensionDevLaunch', value: { label: 'Extension Dev channel launch', milestone: true } },
+            { id: 'siteSubsetLaunch', value: { label: 'Isolate subset of sites launch', milestone: true } },
 
             { id: 'enforceExtSec', value: { label: 'Enforce extension security', owner: 'nick' } },
             { id: 'blockCookies', value: { label: 'Block unauthorized cookies', bug: '467150', owner: 'nick', status: 'complete' } },
             { id: 'isolateExtensions', value: { label: 'Isolate extensions, not all sites', bug: '481066', owner: 'nick' } },
-			
+
             { id: 'sessionHistory', value: { label: 'Session History updated', bug: '236848', owner: 'creis' } },
             { id: 'backForwardBasic', value: { label: 'Basic OOPIF back/forward', bug: '236848', owner: 'creis', status: 'complete' } },
+            { id: 'backForwardFTNs', value: { label: 'Recreate FTNs in back/forward', bug: '502317', owner: 'creis' } },
+            { id: 'backForwardSubtree', value: { label: 'Back/forward with subtree', owner: 'creis' } },
             { id: 'sessionRestoreFNEs', value: { label: 'Restore FNEs in Session Restore', owner: 'avi' } },
-	],
+        ],
         links: [
-            { u: 'oopiframes', v: 'extensionLaunch' },
-            
-            { u: 'extensionLaunch', v: 'enforceExtSec' },
-            { u: 'extensionLaunch', v: 'sessionHistory' },
-			
+            { u: 'extensionTrialLaunch', v: 'enforceExtSec' },
+            { u: 'extensionTrialLaunch', v: 'sessionHistory' },
+
             { u: 'enforceExtSec', v: 'blockCookies' },
             { u: 'enforceExtSec', v: 'isolateExtensions' },
 
             { u: 'sessionHistory', v: 'backForwardBasic' },
+            { u: 'sessionHistory', v: 'backForwardFTNs' },
+            { u: 'sessionHistory', v: 'backForwardSubtree' },
             { u: 'sessionHistory', v: 'sessionRestoreFNEs' },
+
+            { u: 'extensionDevLaunch', v: 'extensionTrialLaunch' },
+
+            { u: 'siteSubsetLaunch', v: 'extensionDevLaunch' },
         ]
     }
 );
