f96511b135999b96d41bdbe0b0a7823f715d5c92
Charles Reis <csreis@users.noreply.github.com>
2015-07-20 11:27:18 -0700

    Update graph-oop-iframes.js

--- graph-oop-iframes.js
+++ graph-oop-iframes.js
@@ -7,21 +7,26 @@ loadData(
             { id: 'siteSubsetLaunch', value: { label: 'Isolate subset of sites launch', milestone: true } },
 
             { id: 'enforceExtSec', value: { label: 'Enforce extension security', owner: 'nick' } },
-            { id: 'blockCookies', value: { label: 'Block unauthorized cookies', bug: '467150', owner: 'nick', status: 'complete' } },
             { id: 'isolateExtensions', value: { label: 'Isolate extensions, not all sites', bug: '481066', owner: 'nick' } },
+            { id: 'blockCookies', value: { label: 'Block unauthorized cookies', bug: '467150', owner: 'nick', status: 'complete' } },
 
             { id: 'sessionHistory', value: { label: 'Session History updated', bug: '236848', owner: 'creis' } },
             { id: 'backForwardBasic', value: { label: 'Basic OOPIF back/forward', bug: '236848', owner: 'creis', status: 'complete' } },
             { id: 'backForwardFTNs', value: { label: 'Recreate FTNs in back/forward', bug: '502317', owner: 'creis' } },
             { id: 'backForwardSubtree', value: { label: 'Back/forward with subtree', owner: 'creis' } },
             { id: 'sessionRestoreFNEs', value: { label: 'Restore FNEs in Session Restore', owner: 'avi' } },
+
+            { id: 'enforceExtSec', value: { label: 'Enforce site isolation', owner: 'nick' } },
+            { id: 'isolateSiteSubset, value: { label: 'Isolate subset of sites', owner: 'nick' } },
+            { id: 'blockPasswords', value: { label: 'Block unauthorized passwords', owner: 'lfg', status: 'complete' } },
+            { id: 'blockCrossSiteDocs', value: { label: 'Block Cross-Site Documents', owner: 'alexmos' } },
         ],
         links: [
             { u: 'extensionTrialLaunch', v: 'enforceExtSec' },
             { u: 'extensionTrialLaunch', v: 'sessionHistory' },
 
-            { u: 'enforceExtSec', v: 'blockCookies' },
             { u: 'enforceExtSec', v: 'isolateExtensions' },
+            { u: 'enforceExtSec', v: 'blockCookies' },
 
             { u: 'sessionHistory', v: 'backForwardBasic' },
             { u: 'sessionHistory', v: 'backForwardFTNs' },
@@ -31,6 +36,11 @@ loadData(
             { u: 'extensionDevLaunch', v: 'extensionTrialLaunch' },
 
             { u: 'siteSubsetLaunch', v: 'extensionDevLaunch' },
+            { u: 'siteSubsetLaunch', v: 'enforceSiteSec' },
+
+            { u: 'enforceSiteSec', v: 'isolateSiteSubset' },
+            { u: 'enforceSiteSec', v: 'blockPasswords' },
+            { u: 'enforceSiteSec', v: 'blockCrossSiteDocs' },
         ]
     }
 );
