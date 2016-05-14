1c22a65a4a9539e683b4c1db24f3c230f6d24ec6
Charles Reis <csreis@users.noreply.github.com>
2016-05-14 15:40:17 -0700

    Add schedule logic.
    Adds ability to display weeks remaining for any task.  Visit the page with the "?schedule" query parameter to enable.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,19 @@
 /*global jQuery, d3, dagreD3, DAG */
 
+// Renders the graph.
+
+// Optionally show schedule for index.html?schedule.
+// Uses query param and not fragment, because it affects how graph is drawn.
+// Fragments would cause in-page navigation and wouldn't repaint graph.
+var queryParams = location.href.split("?");
+var showSchedule = queryParams.length == 2 && queryParams[1] == "schedule";
+if (showSchedule) {
+  document.getElementById("schedule").style.display = "block";
+}
+
+// Make graph available to schedule logic.
+var graph;
+
 (function () {
     'use strict';
     window.DAG = {
@@ -18,10 +32,18 @@
             
             var defaultDrawNodes = renderer._drawNode;
             renderer._drawNode = function(graph, u, root) {
+              if (showSchedule) {
+                // Keep track of the full graph object globally for mouseovers.
+                window.graph = graph;
+                // Depth first, memoized computation of weeks remaining per task.
+                computeSchedule(graph, u);
+              }
+
               defaultDrawNodes(graph, u, root);
               var milestone = graph.node(u).milestone;
-              var status = graph.node(u).status;
               var owner = graph.node(u).owner;
+              var weeksLeft = graph.node(u).weeksLeft;
+              var status = graph.node(u).status;
               var bugNum = graph.node(u).bug;
 
               if (milestone) {
@@ -34,8 +56,19 @@
                   .attr("text-anchor", "left")
                   .attr("class", "owned")
                   .append("tspan")
+                    .attr("dx", "1.5em")
                     .attr("dy", "1.4em")
-                    .text(function() { return "(" + owner + ")"; });
+                    .text(function() { return owner; });
+              }
+              if (showSchedule && weeksLeft) {
+                root
+                  .append("text")
+                  .attr("text-anchor", "left")
+                  .attr("class", "owned")
+                  .append("tspan")
+                    .attr("dx", "-1em")
+                    .attr("dy", "1.4em")
+                    .text(function() { return "" + weeksLeft + "w"; });
               }
               if (status == "complete") {
                 root.attr("class", "complete");
@@ -50,6 +83,10 @@
                     .attr("dx", "-5em")
                     .attr("dy", "1.4em")
                     .text(function() { return bugNum; });
+              }
+              if (showSchedule) {
+                // Show more info on hover.
+                root.attr("onmouseover", "showDetails('" + u + "')");
               }
             };
 
@@ -77,6 +114,57 @@
         }
     };
 })();
+
+function computeSchedule(graph, u) {
+  var node = graph.node(u);
+
+  // Keep track of memoized result to avoid recomputing a given node.
+  if (node.schedule != undefined)
+    return node.schedule;
+  node.schedule = new Object();
+
+  // Add week estimate from this task.
+  var owner = node.owner == undefined ? "Unowned" : node.owner;
+  var weeks = node.weeksLeft == undefined ? 0 : node.weeksLeft;
+  node.schedule[owner] = weeks;
+
+  // Recursively add estimates from any subtasks.
+  var children = graph.successors(u);
+  for (var i = 0; i < children.length; ++i) {
+    var childSchedule = computeSchedule(graph, children[i]);
+    for (var childOwner in childSchedule) {
+      if (node.schedule[childOwner] == undefined)
+        node.schedule[childOwner] = 0;
+      node.schedule[childOwner] += childSchedule[childOwner];
+    }
+  }
+
+  return node.schedule;
+}
+
+function showDetails(u) {
+  var schedule = graph.node(u).schedule;
+  var totalWeeks = 0;
+
+  // Print any owners with open tasks.
+  var details = "<ul>";
+  for (var owner in schedule) {
+    var weeks = schedule[owner];
+    if (weeks > 0) {
+      details += "<li>" + owner + ": " + weeks+ " weeks<br>";
+
+      // Keep track of the max number of weeks for any owner.
+      if (weeks > totalWeeks)
+        totalWeeks = weeks;
+    }
+  }
+  details += "</ul>";
+  var label = graph.node(u).label + "<br>" + totalWeeks + " weeks";
+
+  // Add details to page.
+  document.getElementById("scheduleNode").innerHTML = label;
+  document.getElementById("scheduleDetails").innerHTML = details;
+}
 
 (function () {
     'use strict';
