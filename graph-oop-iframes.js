16a5583e11a294fc13b3ac3e0b6bc5cbae06bfd5
Charles Reis <csreis@users.noreply.github.com>
2016-05-16 15:35:28 -0700

    Update graph-oop-iframes.js

--- graph-oop-iframes.js
+++ graph-oop-iframes.js
@@ -59,6 +59,7 @@ loadData(
 
             // tdiLaunch
             { id: 'netStack', value: { label: 'Downloads and net stack', bug: '472869', owner: 'csharrison', weeksLeft: 4 } },
+            { id: 'androidPaintBug', value: { label: 'Android rendering glitch', bug: '607224', weeksLeft: 2 } },
             { id: 'consolidateProcesses', value: { label: 'Consolidate subframe processes', bug: '512560', owner: 'nick', weeksLeft: 2 } },
             { id: 'screenOrientation', value: { label: 'Screen orientation', bug: '498287', weeksLeft: 4 } },
             { id: 'printing', value: { label: 'OOPIF printing', bug: '455764', owner: 'thestig', weeksLeft: 8 } },
@@ -154,6 +155,7 @@ loadData(
 
             { u: 'tdiLaunch', v: 'extensionLaunch' },
             { u: 'tdiLaunch', v: 'netStack' },
+            { u: 'tdiLaunch', v: 'androidPaintBug' },
             { u: 'tdiLaunch', v: 'consolidateProcesses' },
             { u: 'tdiLaunch', v: 'screenOrientation' },
             { u: 'tdiLaunch', v: 'printing' },
