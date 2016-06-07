bc1ecc6c442d066998fba959b432e730a623f973
Charles Reis <csreis@users.noreply.github.com>
2016-06-07 10:29:18 -0700

    Update graph-oop-iframes.js

--- graph-oop-iframes.js
+++ graph-oop-iframes.js
@@ -41,7 +41,7 @@ loadData(
             { id: 'crossProcessPost', value: { label: 'Cross-process POST submissions', bug: '101395', owner: 'lukasza', weeksLeft: 2 } },
             
             { id: 'taskManagerOrder', value: { label: 'Improve task manager sort order', owner: 'nick', weeksLeft: 1 } },
-            { id: 'secondDisplay', value: { label: 'OOPIF scale on 2nd display', owner: 'wjmaclean', weeksLeft: 1 } },
+            { id: 'secondDisplay', value: { label: 'OOPIF scale on 2nd display', owner: 'wjmaclean', weeksLeft: 2 } },
             { id: 'flashAnimation', value: { label: 'Flash animation', bug: '593520', owner: 'lfg', status: 'complete' } },
             { id: 'pluginDetection', value: { label: 'Plugin detection', bug: '612200', owner: 'alexmos', status: 'complete' } },
             { id: 'dialogs', value: { label: 'Modal dialogs in OOPIFs', bug: '453893', owner: 'avi', status: 'complete' } },
