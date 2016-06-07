aed855f9b0d174fe2cbe4533dc68a40851f5843b
Charles Reis <csreis@users.noreply.github.com>
2016-06-07 16:52:13 -0700

    Update graph-oop-iframes.js

--- graph-oop-iframes.js
+++ graph-oop-iframes.js
@@ -47,7 +47,7 @@ loadData(
             { id: 'taskManagerOrder', value: { label: 'Improve task manager sort order', bug: '616897', owner: 'nick', weeksLeft: 1 } },
             { id: 'secondDisplay', value: { label: 'OOPIF scale on 2nd display', owner: 'wjmaclean', weeksLeft: 2 } },
             { id: 'fileDialogs', value: { label: 'File dialogs in OOPIFs', weeksLeft: 2 } },
-            { id: 'downloadLinks', value: { label: '<a download> links', bug: '596283', owner: 'brettw', weeksLeft: 1 } },
+            { id: 'downloadLinks', value: { label: '&lt;a download&gt; links', bug: '596283', owner: 'brettw', weeksLeft: 1 } },
             { id: 'flashAnimation', value: { label: 'Flash animation', bug: '593520', owner: 'lfg', status: 'complete' } },
             { id: 'pluginDetection', value: { label: 'Plugin detection', bug: '612200', owner: 'alexmos', status: 'complete' } },
             { id: 'dialogs', value: { label: 'Modal dialogs in OOPIFs', bug: '453893', owner: 'avi', status: 'complete' } },
